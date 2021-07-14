71c7b98f6c828a4fd4d6fdb5ed435047c9cfd6f1
balance19 <87434881+balance19@users.noreply.github.com>
2021-07-14 18:16:23 +0200

    Update DS3231_example.py

--- DS3231_example.py
+++ DS3231_example.py
@@ -9,6 +9,9 @@ import time
 #initialisation of RTC object. Several settings are possible but everything is optional. If you meet the standards (see /my_lib/RTC_DS3231.py) no parameter's needed.
 rtc = RTC_DS3231.RTC()
 
+# It is encoded like this sec min hour week day month year
+#rtc.DS3231_SetTime(b'\x00\x14\x18\x28\x14\x07\x21')    #remove comment to set time. Do this only once otherwise time will be set everytime the code is executed.
+
 while True:
     t = rtc.DS3231_ReadTime(1)  #read RTC and receive data in Mode 1 (see /my_lib/RTC_DS3231.py)
     print(t)
